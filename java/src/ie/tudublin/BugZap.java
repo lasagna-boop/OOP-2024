62475f639db4a705d5faec3ae818ff374fd39051
Stevie <D20124623@mytudublin.ie>
2024-01-31 16:45:56 +0000

    i made a circle move

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -8,15 +8,19 @@ public class BugZap extends PApplet {
 	}
 
 	public void setup() {
-		
+		colorMode (RGB); 
+		background(0);
 	}
-
+	
+	float x = 400;
 
 	public void keyPressed()
 	{
 		if (keyCode == LEFT)
 		{
 			System.out.println("Left arrow pressed");
+			x += -20;
+			
 		}
 		if (keyCode == RIGHT)
 		{
@@ -31,5 +35,9 @@ public class BugZap extends PApplet {
 	int gameMode = 0;
 
 	public void draw() {
+
+		circle(15, 30, 45);
+		circle (x, 560, 90); 
+	
 	}
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -11,22 +11,7 @@ public class Main
         processing.core.PApplet.runSketch( a, new BugZap());
     }
 
-	public void doDogStuff()
-	{
-		// Create a new Dog called Misty
-		// Call the constructor
-		Dog misty = new Dog("Misty");
-		misty.speak();
 
-        Dog lucy = new Dog("Lucy");
-		lucy.speak();
-	}
-
-	public void doCatStuff()
-	{
-		// Put your code here
-		System.out.println("cats are from space");
-	}
 	
 	public static void main(String[] args)
 	{
@@ -35,30 +20,6 @@ public class Main
 		
 		Main m = new Main();
 
-		m.doDogStuff();
-
-		m.doCatStuff();
-
-		Dog nuala = new Dog("Nuala");
-		nuala.speak();
-
-		Cat atuma = new Cat("Atuma");
-		atuma.speak();
-
-		Animal larry = new Dog("Larry");
-		larry.speak();
-
-
-		larry = new Cat("TopCat");
-		larry.speak();
-
-		Animal a = new Dog("A");
-		Animal b = new Dog("B");
-		Animal c = a;
-		a.setName("G");
-
-		System.out.println(a.getName());
-		System.out.println("C: " + c.getName());
 		
 
 
