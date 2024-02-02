97374e1a36521880a7267e2a7af3f64bb9379fb2
Stevie <D20124623@mytudublin.ie>
2024-02-02 15:50:17 +0000

    major improvements thanks Ruan

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -3,67 +3,91 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class BugZap extends PApplet {
+	float playerWidth, playerX, playerY, bugX, bugY, bugWidth;
 	public void settings() {
-		size(800, 800);
+		size(500, 500);
 	}
 
 	public void setup() {
 		colorMode (RGB); 
 		background(0);
+		playerWidth = 20;
+		playerX = width/2;
+		playerY = height - (playerWidth/2);
+
+		bugX = random(0 + bugWidth, width - bugWidth);
+		bugY = 0 + bugWidth/2;
+		bugWidth = 50;
 	}
-	
-	float playerX = 400;
-	float playerY = 400; 
 
 	public void keyPressed()
 	{
 		if (keyCode == LEFT)
 		{
-			System.out.println("Left arrow pressed");
-			playerX += -20;
+			if(playerX > (0 + playerWidth)){
+				playerX -= 20;
+			}
 			
 		}
 		if (keyCode == RIGHT)
 		{
-			System.out.println("Right arrow pressed");
-			playerX += 20 ; 
+			if(playerX < (width - playerWidth)){
+				playerX += 20; 
+			}
 		}
-		if (keyCode == UP)
+		if (keyCode == ' ')
 		{ 
-			System.out.println("U UP");
-			playerY += -20 ;
-		}
-		if (keyCode == DOWN) 
-		{	
-			System.out.println("U DOWN") ; 
-			playerY += 20 ; 
+			line(playerX, (playerY-playerWidth/2), playerX, 0);
 		}
 	}
 
 	int gameMode = 0;
 
 	//separate function for player
-	public void drawPlayer() {
+	public void drawPlayer(float x, float y, float w) {
+		float h = w/2;
+		float LeftX, RightX, TopY, BottomY;
+		noFill();
+		stroke(255);
+		
+		LeftX = (x - w/2);
+		RightX = (x + w/2);
+		TopY = (y - h/2);
+		BottomY = (y + h/2);
+
+		line(LeftX, TopY, RightX, TopY);
+		line(LeftX, BottomY, RightX, BottomY);
+		line(RightX, TopY, RightX, BottomY);
+		line(LeftX, TopY, LeftX, BottomY); 
 	
 	}
 
 
 	//separate function for bug
-	public void drawBug(){
-	square(random (0, 800),random (0, 800),50);
+	public void drawBug(float x, float y, float w){
+		square(x, y, w);
 	}
 
 
 	//void draw 
 	public void draw() {
-	rect(playerX, playerY, 90, 90);
+		background(0);
 
-	stroke(1, 0, 255);
-	circle(500, 450 , 50) ; 
+		if ((frameCount % 60) == 0 ) {
+			bugX += random(-30, 30);
+			bugY += random(5, 30);
 
-	if ((frameCount%60 == 0 )) {
-	drawBug();
-	}
+			if (bugX < (0+bugWidth)){
+				bugX = bugWidth; 
+			} 
+			else if (bugX > (width - bugWidth)) {
+				bugX = width - bugWidth;
+			}
+		 
 
+		}
+		
+		drawBug(bugX, bugY, bugWidth);
+		drawPlayer(playerX, playerY, playerWidth);
 	}
 }
